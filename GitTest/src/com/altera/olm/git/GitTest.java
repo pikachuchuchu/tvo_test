08016e3bdb1a15c463d158de86b8593ecb8c0c2a
tvo <tvo@altera.com>
2013-11-01 12:45:52 -0700

    changed

--- GitTest/src/com/altera/olm/git/GitTest.java
+++ GitTest/src/com/altera/olm/git/GitTest.java
@@ -2,6 +2,6 @@ package com.altera.olm.git;
 
 public class GitTest {
 	public static void main(String[] args) {
-    System.out.println("Git is fun");
+    System.out.println("Git is fun, but not cool.");
   }
 }
